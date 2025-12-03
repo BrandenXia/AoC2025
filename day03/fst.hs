e81f99334502ac1342c804f8e2f02020ef524797
BrandenXia <xxtbranden@outlook.com>
2025-12-03 10:28:55 -0800

    make day 3 code look better

--- day03/fst.hs
+++ day03/fst.hs
@@ -4,10 +4,10 @@ import Data.Char (digitToInt)
 import Data.Foldable (maximumBy)
 
 fmax :: [Int] -> Int
-fmax xs =
-  let cmp (i1, x1) (i2, x2) = compare x1 x2 <> compare i2 i1
-      (idx, e) = maximumBy cmp . zip [1 ..] $ init xs
-   in e * 10 + maximum (drop idx xs)
+fmax xs = e * 10 + maximum (drop idx xs)
+  where
+    cmp (i1, x1) (i2, x2) = compare x1 x2 <> compare i2 i1
+    (idx, e) = maximumBy cmp . zip [1 ..] $ init xs
 
 main :: IO ()
 main = do
--- day03/snd.hs
+++ day03/snd.hs
@@ -4,12 +4,12 @@ import Data.Char (digitToInt)
 import Data.Foldable (maximumBy)
 
 fmax :: [Int] -> Int
-fmax xs =
-  let cmp (i1, x1) (i2, x2) = compare x1 x2 <> compare i2 i1
-      step (prev, pidx) curr = (prev * 10 + e, idx + pidx)
-        where
-          (idx, e) = maximumBy cmp . zip [1 ..] . drop pidx $ take (length xs - curr) xs
-   in fst . foldl step (0, 0) $ reverse [0 .. 11]
+fmax xs = fst . foldl step (0, 0) $ reverse [0 .. 11]
+  where
+    cmp (i1, x1) (i2, x2) = compare x1 x2 <> compare i2 i1
+    step (prev, pidx) curr =
+      let (idx, e) = maximumBy cmp . zip [1 ..] . drop pidx $ take (length xs - curr) xs
+       in (prev * 10 + e, idx + pidx)
 
 main :: IO ()
 main = do
