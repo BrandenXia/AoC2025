344e03143acc5bffec337fb94ab2a1a2ab76a083
BrandenXia <xxtbranden@outlook.com>
2025-12-15 15:24:38 -0800

    remove space

--- day12/fst.c
+++ day12/fst.c
@@ -4,7 +4,7 @@
 
 void mv(void **p) { *p = (char *)*p + 1; }
 #define lnb(a, p, b) do { *(a) = *(char *)*(p) - 0x30; for (mv(p); *(char *)*(p) != (b); mv(p)) { *(a) *= 0xa; *(a) += *(char *)*(p) - 0x30; } } while(0)
-#define lnbb(a, p, b1, b2) do { *(a) = *(char *)*(p) - 0x30;  for (mv(p); *(char *)*(p) != (b1) && *(char *)*(p) != (b2); mv(p)) { *(a) *= 0xa; *(a) += *(char *)*(p) - 0x30; } } while(0)
+#define lnbb(a, p, b1, b2) do { *(a) = *(char *)*(p) - 0x30; for (mv(p); *(char *)*(p) != (b1) && *(char *)*(p) != (b2); mv(p)) { *(a) *= 0xa; *(a) += *(char *)*(p) - 0x30; } } while(0)
 void rd(void **p, int *a, int *b) { lnb(a, p, 0x78); mv(p); lnb(b, p, 0x3a); }
 int ct(void **p, int *c) { int t = 0, i = 0, m; while (*((char *)*p - 1) != 0xa) { lnbb(&m, p, 0x20, 0xa); mv(p); t += m * (i++)[c]; } return t; }
 
